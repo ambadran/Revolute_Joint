a82db5ed2f6e3624dcb4960dec4d54914b5ccfc7
Abdulrahman Badran <ambadran717@hotmail.com>
2024-02-22 23:59:07 +0200

    further development in H-bridge circuit

--- Programming/micropython/step_motor.py
+++ Programming/micropython/step_motor.py
@@ -1,23 +1,24 @@
 from machine import Pin, PWM
+from micropython import const
+
 
 class Motor:
     '''
     Instance to control a stepper motor driver through repl easily
     '''
+    CW_STATE = 1
+    CCW_STATE = 0
     def __init__(self):
         self.step = PWM(Pin(14))
         self.step.freq(20)
         self.step.duty_u16(0)
 
-        self.dir = Pin(15, Pin.OUT)
-        self.dir.off()  # start as default cw value which is 0
+        self.dir_pin = Pin(15, Pin.OUT)
+        self.dir_pin.off()  # start as default cw value which is 0
         
         self.enable = Pin(13, Pin.OUT)
         # self.enable.off()
         self.enable.on()
-
-        self.cw_value = False
-        self.ccw_value = True
 
     def on(self):
         # self.enable.on()
@@ -29,24 +30,39 @@ class Motor:
         self.enable.on()
         self.step.duty_u16(0)
 
+    def unhold(self):
+        self.enable.off()
+        self.step.duty_u16(32628)
+
+    def hold(self):
+        self.enable.off()
+        self.step.duty_u16(0)
+
     def freq(self, frequency):
         self.step.freq(frequency)
 
     def cw(self):
-        self.dir.value(self.cw_value)
+        self.dir_pin.value(self.CW_STATE)
 
     def ccw(self):
-        self.dir.value(self.ccw_value)
+        self.dir_pin.value(self.CCW_STATE)
 
     @property
-    def dir_value(self):
-        return self.dir.value()
+    def direction(self):
+        return self.dir_pin.value()
+
+    @direction.setter
+    def direction(self, value):
+        self.dir_pin.value(value)
+
+    def toggle(self):
+        self.dir_pin.toggle()
 
     def invert_default_dir_values(self):
-        self.cw_value = not self.cw_value
-        self.ccw_value = not self.ccw_value
+        self.CW_STATE = self.CW_STATE ^ 1
+        self.CCW_STATE = self.CCW_STATE ^ 1
 
-        self.dir.toggle()
+        self.toggle()
 
 
 motor = Motor()
